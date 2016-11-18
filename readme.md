dbd823f874bf1ff1dd8f419309e97998a484ed6e
tamami <tamami.oka@gmail.com>
2016-11-18 15:19:58 +0700

    init commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Belajar spring-boot Rest
+
+bahannya dari sini -> https://spring.io/guides/tutorials/bookmarks/
---
+++ src/main/java/lab/aikibo/bookmarks/Account.java
@@ -0,0 +1,8 @@
+package lab.aikibo.bookmarks;
+
+/**
+ * Created by tamami on 18/11/16.
+ */
+@Entity
+public class Account {
+}
